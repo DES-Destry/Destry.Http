99afb1e9f0e3e4671b7d619cf2148815df16961c
Andrey Kirik <dev.andrey.kirik@gmail.com>
2024-01-07 16:18:48 +0600

    feat: setup base url and request path in sender

--- Destry.Http/Sender.cs
+++ Destry.Http/Sender.cs
@@ -2,10 +2,11 @@ namespace Destry.Http;
 
 public abstract class Sender
 {
+    public abstract void SetBaseUrl(string url);
     public abstract void AddQuery(string name, string value);
     public abstract void AddHeader(string name, string value);
     public abstract void AddParam(string name, string value);
     public abstract void SetBody(object body);
 
-    public abstract Task<T> ExecuteAsync<T>(string httpMethod);
+    public abstract Task<T> SendHttpRequestAsync<T>(string httpMethod, string path);
 }
--- Destry.Http/Senders/HttpClientSender.cs
+++ Destry.Http/Senders/HttpClientSender.cs
@@ -5,15 +5,18 @@ public sealed class HttpClientSender : Sender
     private readonly Dictionary<string, string> _headers = new();
     private readonly Dictionary<string, string> _params = new();
     private readonly Dictionary<string, string> _queries = new();
+    private string _baseUrl;
     private object? _body;
 
+    public override void SetBaseUrl(string url) => _baseUrl = url;
     public override void AddQuery(string name, string value) => _queries.Add(name, value);
     public override void AddHeader(string name, string value) => _headers.Add(name, value);
     public override void AddParam(string name, string value) => _params.Add(name, value);
     public override void SetBody(object body) => _body = body;
 
-    public override async Task<T> ExecuteAsync<T>(string httpMethod)
+    public override async Task<T> SendHttpRequestAsync<T>(string httpMethod, string path)
     {
+        var method = HttpMethod.Parse(httpMethod);
         throw new NotImplementedException();
     }
 }
