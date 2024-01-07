e00da47c839584e12d9890b393e4f9125b6fd7fa
Andrey Kirik <dev.andrey.kirik@gmail.com>
2024-01-07 16:05:17 +0600

    feat: add data attaching in sender

--- Destry.Http/Sender.cs
+++ Destry.Http/Sender.cs
@@ -5,6 +5,7 @@ public abstract class Sender
     public abstract void AddQuery(string name, string value);
     public abstract void AddHeader(string name, string value);
     public abstract void AddParam(string name, string value);
+    public abstract void SetBody(object body);
 
-    public abstract Task<T> ExecuteAsync<T>();
+    public abstract Task<T> ExecuteAsync<T>(string httpMethod);
 }
--- Destry.Http/Senders/HttpClientSender.cs
+++ Destry.Http/Senders/HttpClientSender.cs
@@ -2,20 +2,18 @@ namespace Destry.Http.Senders;
 
 public sealed class HttpClientSender : Sender
 {
-    public override void AddQuery(string name, string value)
+    private readonly Dictionary<string, string> _headers = new();
+    private readonly Dictionary<string, string> _params = new();
+    private readonly Dictionary<string, string> _queries = new();
+    private object? _body;
+
+    public override void AddQuery(string name, string value) => _queries.Add(name, value);
+    public override void AddHeader(string name, string value) => _headers.Add(name, value);
+    public override void AddParam(string name, string value) => _params.Add(name, value);
+    public override void SetBody(object body) => _body = body;
+
+    public override async Task<T> ExecuteAsync<T>(string httpMethod)
     {
         throw new NotImplementedException();
     }
-
-    public override void AddHeader(string name, string value)
-    {
-        throw new NotImplementedException();
-    }
-
-    public override void AddParam(string name, string value)
-    {
-        throw new NotImplementedException();
-    }
-
-    public override async Task<T> ExecuteAsync<T>() { throw new NotImplementedException(); }
 }
