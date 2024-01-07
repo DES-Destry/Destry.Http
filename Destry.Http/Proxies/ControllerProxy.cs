dd1b9e26b3ecf8484a6f87753633e6bd7b99a673
Andrey Kirik <dev.andrey.kirik@gmail.com>
2024-01-07 17:39:50 +0600

    feat: call http request in proxy
    Without data passing.

--- Destry.Http/Proxies/ControllerProxy.cs
+++ Destry.Http/Proxies/ControllerProxy.cs
@@ -1,8 +1,9 @@
 using System.Reflection;
+using Destry.Http.Methods;
 
 namespace Destry.Http.Proxies;
 
-internal class ControllerProxy<T> : DispatchProxy where T : class
+internal sealed class ControllerProxy<T> : DispatchProxy where T : class
 {
     private string _baseUrl;
     private Sender _sender;
@@ -13,8 +14,28 @@ internal class ControllerProxy<T> : DispatchProxy where T : class
         _sender = sender;
     }
 
-    protected override object? Invoke(MethodInfo? targetMethod, object?[]? args)
+    protected override async Task<object?> Invoke(MethodInfo? targetMethod, object?[]? args)
     {
-        throw new NotImplementedException();
+        ArgumentNullException.ThrowIfNull(targetMethod);
+
+        var sendAttribute = targetMethod.GetCustomAttribute<SendAttribute>(true);
+
+        if (sendAttribute is null)
+            throw new Exception(
+                "To make request callable you must use any of Send attribute on method"); // TODO: NotSendableMethodException
+
+        var returnType = targetMethod.ReturnType;
+
+        var sendHttpRequestMethod =
+            _sender.GetType().GetMethod("SendHttpRequestAsync")!.MakeGenericMethod([returnType]);
+
+
+        //return await _sender.SendHttpRequestAsync<T>(
+        // sendAttribute.Method.Method,
+        // sendAttribute.Path); - with dynamic T
+        return sendHttpRequestMethod.Invoke(_sender, [
+            sendAttribute.Method.Method,
+            sendAttribute.Path
+        ]);
     }
 }
