ef9a77f2b82be2cd9505a78516a5d8819af0103a
Andrey Kirik <dev.andrey.kirik@gmail.com>
2024-01-29 00:37:19 +0600

    docs: xml docs for controller attribute

--- Destry.Http/Controller/ControllerAttribute.cs
+++ Destry.Http/Controller/ControllerAttribute.cs
@@ -1,13 +1,28 @@
 namespace Destry.Http.Controller;
 
+/// <summary>
+///     You can add this attribute to say, that this interface can be used by Destry.Http.
+/// </summary>
 [AttributeUsage(AttributeTargets.Interface)]
 public sealed class ControllerAttribute : Attribute
 {
-    public string? BaseUrl { get; set; }
-    
+    /// <summary>
+    ///     Default Destry.Http controller.
+    /// </summary>
+    /// <remarks>
+    ///     You can delete this attribute without any problems. But I'll recommend to use this to say "hey, this interface is
+    ///     using by Destry.Http to build custom Http Client" to other programmers.
+    /// </remarks>
     public ControllerAttribute() { }
-    public ControllerAttribute(string baseUrl)
-    {
-        BaseUrl = baseUrl;
-    }
+
+    /// <summary>
+    ///     Create custom Http client with specified base URL.
+    /// </summary>
+    /// <param name="baseUrl">
+    ///     It's a first part of the path of any HTTP request. <br />
+    ///     For example: https://api.some-cool-service.com
+    /// </param>
+    public ControllerAttribute(string baseUrl) { BaseUrl = baseUrl; }
+
+    internal string? BaseUrl { get; private set; }
 }
