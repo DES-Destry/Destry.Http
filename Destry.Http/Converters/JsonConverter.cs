45f882cd24d41d178fb6ba09b31940f672720dcc
Andrey Kirik <dev.andrey.kirik@gmail.com>
2024-02-04 18:32:42 +0600

    fix: json converter - parse json camel case fields

--- Destry.Http/Converters/JsonConverter.cs
+++ Destry.Http/Converters/JsonConverter.cs
@@ -4,13 +4,18 @@ namespace Destry.Http.Converters;
 
 internal sealed class JsonConverter : Converter
 {
+    private readonly JsonSerializerOptions _options = new()
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+    };
+
     public override async Task<T?> FromRawResponseToAsync<T>(Task<HttpRawResponse> response)
         where T : default
     {
         var awaited = await response;
 
         if (!awaited.IsError)
-            return await Task.FromResult(JsonSerializer.Deserialize<T>(awaited.Data!));
+            return await Task.FromResult(JsonSerializer.Deserialize<T>(awaited.Data!, _options));
 
         if (awaited.IsError)
             throw awaited.Exception!;
