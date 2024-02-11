f241b5d3d3e5f13a16fb313725f78f0b3dd23b04
Andrey Kirik <dev.andrey.kirik@gmail.com>
2024-02-11 23:03:46 +0600

    feat: display names for tests

--- Destry.Http.UnitTests/JsonPlaceholderTest.cs
+++ Destry.Http.UnitTests/JsonPlaceholderTest.cs
@@ -9,7 +9,7 @@ public class JsonPlaceholderTest(ITestOutputHelper output)
     private readonly IJsonPlaceholderController _jsonPlaceholderController =
         new ControllerBuilder().From<IJsonPlaceholderController>();
 
-    [Fact]
+    [Fact(DisplayName = "It should work")]
     public async Task ItShouldWork()
     {
         var response = (await _jsonPlaceholderController.GetAllPosts())?.ToImmutableArray() ?? [];
@@ -17,7 +17,7 @@ public class JsonPlaceholderTest(ITestOutputHelper output)
         Assert.Equal(100, response.Length);
     }
 
-    [Fact]
+    [Fact(DisplayName = "Send a request with path parameter")]
     public async Task ItShouldSendRequestWithPathParam()
     {
         var post = await _jsonPlaceholderController.GetPostById(1);
